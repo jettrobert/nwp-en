b9410c790f5020ac76204840183f49ae3b857229
Robert Jett <me@robertjett.com>
2023-09-12 17:34:54 +0200

    Totally changed the blog template. Going to test.

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,5 +1,6 @@
 import GhostContentAPI from '@tryghost/content-api';
 import Link from 'next/link';
+import React, { useRef } from 'react';
 
 // Initialize the Ghost API
 const api = new GhostContentAPI({
@@ -18,7 +19,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const post = await api.posts.read({ slug: params.slug });
+    const post = await api.posts.read({ slug: params.slug }, { include: 'tags' });
 
     return {
         props: { post },
@@ -26,6 +27,22 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Post({ post }) {
+
+    console.log(post);  // This will log the tags to the console
+
+    const imageURL = `/cover-photos/${post.slug}-cover.png`;
+
+    const textRef = useRef(null);
+
+    const scrolltoText = () => {
+        const offset = 10; // Adjust this value according to your needs
+        const textPosition = textRef.current.offsetTop;
+        window.scrollTo({
+            top: textPosition - offset,
+            behavior: "smooth"
+        });
+    };
+    
     return (
         <>
             <meta charSet="UTF-8" />
@@ -34,28 +51,25 @@ export default function Post({ post }) {
             <link rel="stylesheet" href="https://use.typekit.net/zkz4rdl.css" />
             <div className="logo-container">
                 <Link href="/">
-                        <img src="/logo-white.svg" alt="Home" />
+                    <img src="/logo-white.svg" alt="Home" />
                 </Link>
-
             </div>
-            <div className="blog-post">
-                <h1 className="title">{post.title}</h1>
-                <p className="date">{new Date(post.published_at).toLocaleDateString()}</p>
-                <img className="cover-photo" src={post.feature_image} alt="Cover Photo" />
-                <div className="markdown-content" dangerouslySetInnerHTML={{ __html: post.html }}></div>
-                <div className="related-links">
-                    <a href="#" className="link-box">
-                        Link 1
-                    </a>
-                    <a href="#" className="link-box">
-                        Link 2
-                    </a>
-                    <a href="#" className="link-box">
-                        Link 3
-                    </a>
+            <div className="cover-container">
+                <img className="cover-photo" src={imageURL} alt="Cover Photo" />
+                <h1 className="cover-title">{post.title}</h1>
+                <p className="cover-excerpt">{post.excerpt}</p>
+                <button className="read-more-button" onClick={scrolltoText}>Read more</button>
+            </div>
+            <div className="text-section" ref={textRef}>
+                <div className="metadata-container">
+                    <p className="publication-date">{new Date(post.published_at).toLocaleDateString()}</p>
+                    {post.tags && post.tags.map((tag, index) => (
+                        <span key={index} className="publication-tags">{tag.name}</span>
+                    ))}
                 </div>
-                <footer>New World Person 2023</footer>
+                <div className="markdown-content" dangerouslySetInnerHTML={{ __html: post.html }}></div>
             </div>
+
         </>
     );
 }
