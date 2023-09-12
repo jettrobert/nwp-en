98fb47768ad9f53e5f73f81b1385ca66bd2ca93c
Robert Jett <me@robertjett.com>
2023-09-12 17:34:20 +0200

    Completely redesigned the blog.

--- pages/index.js
+++ pages/index.js
@@ -135,6 +135,7 @@ export async function getStaticProps() {
   try {
     posts = await api.posts.browse({
       limit: 15, // Feel free to adjust the limit
+      filter: 'tags: [en]'
     });
   } catch (error) {
     console.error(error);
